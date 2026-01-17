47af8bb4d133987a43a0e91d184e6ef3c120c616
sandipbera9211 <sandipbera9636@gmail.com>
2026-01-17 11:44:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,47 +1,44 @@
 import express from "express";
-import cors from "cors"
-import 'dotenv/config'
+import cors from "cors";
+import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js'
+import authRouter from './routes/authRoutes.js';
 import userRouter from "./routes/userRoute.js";
 
 const app=express();
 const port=process.env.PORT || 4000;
 
+// Connect DB
 connectDB();
 
+
 const allowedOrigins = [
-  'https://authentication-frontend-r49s.onrender.com',
-  'http://localhost:5173'
+  'http://localhost:5173', // dev
+  'https://authentication-frontend-6qhm.onrender.com' // prod frontend
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
-
-app.options('*', cors());
 
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true); // allow Postman / server-side requests
+    if(allowedOrigins.indexOf(origin) === -1){
+      return callback(new Error('CORS Policy Blocked this origin'), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
-
-//Api EndPoints.
-app.get('/',(req,res)=> res.send("Api working..."))
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
+// Routes
+app.get('/', (req, res) => res.send("API working..."));
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 
 app.listen(port,()=> console.log(`server started on Port:${port}`))
+
+
